96ab6fb0786486dcb76f86cc23b599633e3f90a1
fosterv2 <fosterv2@outlook.com>
2020-09-17 17:10:32 -0700

    Delete unnecessary files and add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+author1 = Author.create(name: "J. K. Rowling", age: 55)
+author2 = Author.create(name: "Philip Pullman", age: 73)
+
+Book.create(title: "Harry Potter and the Sorcerer's Stone", author: author1)
+Book.create(title: "Harry Potter and the Chamber of Secrets", author1)
+Book.create(title: "The Golden Compass", author: author2)
+Book.create(title: "The Subtle Knife", author: author2)
